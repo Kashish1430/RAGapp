e033d1cd09ccb3aa23378f37fa580b06f6896ce0
Kashish1430 <kashishrajput95@yahoo.com>
2024-07-10 11:39:29 +0530

    Docuemnt Loader Class

--- src/Database/document_loader.py
+++ src/Database/document_loader.py
@@ -1,14 +1,33 @@
 from langchain_community.document_loaders import PyPDFLoader
 from src.utils import get_files_path
 
-files = get_files_path('Books')
+class DocumentLoader():
+    def __init__(self):
+        self.books = get_files_path('Books')
+        
+    def __len__(self):
+        return len(self.books)
+    
+    def load_doc(self, doc):
+        return PyPDFLoader(doc).load()
+    
+    def load_all_docs(self, books):
+        document_dict = {}
+        for i in range(len(books)):
+            loader = PyPDFLoader(books[i])
+            document_dict[books[i]] = loader.load()
+        return document_dict
 
-doc_loader = PyPDFLoader(files[0])
-doc = doc_loader.load()
-
-print(doc[0].page_content)
-print('Source: ',doc[0].metadata['source'])
-print('Page: ',doc[0].metadata['page'])
-
-print('Combined Source: ', str(doc[0].metadata['source']) + str(doc[0].metadata['page']))
-print('Number of Pages: ', len(doc))
+if __name__ == '__main__':
+    doc_loader = DocumentLoader()
+    print(doc_loader.books)
+    print(len(doc_loader))
+    #print(doc_loader.load_doc(doc_loader.books[0]))
+    
+    all_docs = doc_loader.load_all_docs(doc_loader.books)
+    print(all_docs.keys())
+    print(len(all_docs[next(iter(all_docs.keys()))]))
+    print(all_docs[next(iter(
+        all_docs.keys()))][0].page_content)
+    print(all_docs[next(iter(
+        all_docs.keys()))][0].metadata['page'])
